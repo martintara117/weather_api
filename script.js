887034f594d5823dc58e28cbe9b1121b0599d9cb
Tara Martin <taramartin@Taras-MacBook-Pro.local>
2020-10-09 10:32:45 -0400

    changed label font size, added padding, border radius and background color to input, added font size, padding border and border radius to nav button and added display, vertical align and margin to the nav and main.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let apiKey = "85b394aecf7ef94b9d97722dd2a1d6b1";
 let queryUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=`;
-let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&q=`;
+let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&q=`;
 
 $("nav > button").on("click", function () {
   let city = $("nav input").val();
@@ -15,12 +15,7 @@ function citySearch(city) {
   $.ajax({
     url: forecastUrl + city,
     method: "GET",
-  }).then((forecastResponse) => {
-    console.log(forecastResponse);
-    for (i = 0; i < 40; i += 8) {
-      console.log(forecastResponse.list[i]);
-    }
-  });
+  }).then(forecastWeather);
 }
 function currentWeather(response) {
   let city = response.name;
@@ -29,8 +24,27 @@ function currentWeather(response) {
   let windSpeed = response.wind.speed;
   $("main section:first-child").html(`
     <h2>${city}</h2>
-    <p>Temperature: ${temp}</p>
-    <p>Humidity: ${humidity}</p>
-    <p>Wind Speed: ${windSpeed}</p>
+    <p>Temperature: ${temp}&deg;F</p>
+    <p>Humidity: ${humidity}%</p>
+    <p>Wind Speed: ${windSpeed} MPH</p>
   `);
+}
+function forecastWeather(response) {
+  let html = "<h3>Five-Day Forecast</h3>";
+  for (let i = 0; i < response.list.length; i += 8) {
+    let day = response.list[i];
+    let date = day.dt_txt;
+    let icon = day.weather[0].icon;
+    let temp = day.main.temp;
+    let humidity = day.main.humidity;
+    html += `
+      <figure> 
+        <h5>${date}</h5>
+        <img src="http://openweathermap.org/img/wn/${icon}@2x.png" />
+        <p>Temperature: ${temp}&deg;F</p>
+        <p>Humidity: ${humidity}%</p>
+      </figure>
+    `;
+  }
+  $("main section:last-child").html(html);
 }
